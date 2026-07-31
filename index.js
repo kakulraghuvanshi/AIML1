6947268ef635273a6f02dd78f22a6c4b0fb9137d
kakulraghuvanshi <kakulraghuvanshi9@gmail.com>
2026-07-31 10:53:50 +0530

    Add files via upload
    done

---
+++ index.js
@@ -0,0 +1,29 @@
+const app=require('./app.js');
+// let a=20;
+// let b=20;
+// let c=20;
+// console.log(a+b+c);
+// console.log(a);
+// console.log(b+c);
+
+// var x=20;
+// if(x==20){
+//     console.log("x is 20");
+// }
+// else{
+//     console.log("x is not 20");
+// }
+
+// for(var i=0;i<5;i++){
+//     console.log(i);
+// }
+
+// var x=20;
+// const arr=[1,2,3,4,5];
+// for(var i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+//     // console.log(arr[2]);
+// }
+
+
+console.log(app.xz)
